984d721bcdbb55c8627050c23144f76fdd94562e
Mirelle Mota <mirelle.motta06@gmail.com>
2023-04-12 22:45:51 -0300

    Let's Code - Python

---
+++ condicionais.py
@@ -0,0 +1,10 @@
+media = float(input('Informe a nota do estudante: '))
+
+if media >= 7:
+    print('Você foi aprovado')
+    
+elif media >= 5:
+    print('Recuperação')
+    
+else:
+    print('Você foi reprovado')
